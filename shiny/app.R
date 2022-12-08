357afa57289cc25fef8d6ca9df517b55914bb7ab
FabianOrduna <forduafe@itam.mx>
2022-12-08 02:22:20 -0600

    Agrego visualización

--- shiny/app.R
+++ shiny/app.R
@@ -332,6 +332,30 @@ server <- function(input, output, session) {
     eliminaCasaForm$server(input,output)
     actualizaForm$server(input,output)
     buscaCasaForm$server(input,output)
+    
+    
+    #res <- GET("http://localhost:5000/houses")
+    #jsonResult <- fromJSON(content(res, "text"))
+    #prices <- jsonResult$price
+    output$lectura <- shiny::renderText({
+      "tio"
+    })
+    
+    output$distPlot <- renderPlot({
+
+      res <- GET("http://api:5000/houses")
+      jsonResult <- fromJSON(content(res, "text"))
+      prices <- jsonResult$price
+      
+    #x    <- faithful$waiting
+      x <- prices
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+    hist(x, breaks = bins, col = "#FF8C00", border = "white",
+         xlab = "Precios de las casas (en dólares)",
+         main = "Histograma de precios")
+
+    })
     # More server logic
 }
 
@@ -344,9 +368,24 @@ ui <- shiny::bootstrapPage(
     shinyreforms::shinyReformsPage(  # This adds a dependency on shinyreforms .css
         shiny::fluidPage(
           
-          
-          fluidRow(
-            
+          tags$style(HTML(".js-irs-0 .irs-single, .js-irs-0 .irs-bar-edge, .js-irs-0 .irs-bar {background: #FF8C00}")),
+          fluidRow(            
+            column(6,
+                wellPanel(
+                    plotOutput(outputId = "distPlot"),
+                    sliderInput(inputId = "bins",
+                  label = "Total de particiones:",
+                  min = 1,
+                  max = 60,
+                  value = 30),
+                     shiny::htmlOutput("lectura")
+                )
+            ),
+            column(6,
+                wellPanel(
+                    
+                )
+            ),
             column(4,
                    wellPanel(
                      
@@ -377,80 +416,11 @@ ui <- shiny::bootstrapPage(
                      shiny::htmlOutput("eliminaresult")
                    )
             ),
-
-
-
-
-
-
-
-
-
           )
         )
     )
 )
 
-
-
-
-# Define UI for app that draws a histogram ----
-#ui <- fluidPage(
-
-  # App title ----
- # titlePanel("Hello Shiny!"),
-
-  # Sidebar layout with input and output definitions ----
-  #sidebarLayout(
-
-    # Sidebar panel for inputs ----
-   # sidebarPanel(
-
-      # Input: Slider for the number of bins ----
-    #  sliderInput(inputId = "bins",
-     #             label = "Number of bins:",
-      #            min = 1,
-       #           max = 50,
-        #          value = 30)
-
-    #),
-
-    # Main panel for displaying outputs ----
-    #mainPanel(
-
-      # Output: Histogram ----
-     # plotOutput(outputId = "distPlot")
-
-      
-    #)
-  #)
-#)
-
-# Define server logic required to draw a histogram ----
-#server <- function(input, output) {
-
-  # Histogram of the Old Faithful Geyser Data ----
-  # with requested number of bins
-  # This expression that generates a histogram is wrapped in a call
-  # to renderPlot to indicate that:
-  #
-  # 1. It is "reactive" and therefore should be automatically
-  #    re-executed when inputs (input$bins) change
-  # 2. Its output type is a plot
-  
-  
-  #$distPlot <- renderPlot({
-
-   # x    <- faithful$waiting
-    #bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    #hist(x, breaks = bins, col = "#007bc2", border = "white",
-     #    xlab = "Waiting time to next eruption (in mins)",
-      #   main = "Histogram of waiting times")
-
-    #})
-
-#}
 
 # Run the application 
 shinyApp(ui = ui, server = server)
